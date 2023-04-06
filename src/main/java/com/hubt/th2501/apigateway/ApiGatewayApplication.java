043aa7848e5dce2f8079f878c60474d75e322531
Nguyen Huu Quan <huuquan.nguyen.qt@gmail.com>
2023-04-06 21:17:00 +0700

    api-gateway

---
+++ src/main/java/com/hubt/th2501/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hubt.th2501.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubt/th2501/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubt.th2501.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
